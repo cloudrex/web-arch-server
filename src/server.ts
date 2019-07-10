89063af58a297821fd365645d351901dc3c7aba1
CloudRex <cloudrex@outlook.com>
2019-07-10 11:32:22 -0400

    Minor syntax enhancements

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import {ApolloServer, gql} from "apollo-server";
 // This is a (sample) collection of books we"ll be able to query
 // the GraphQL server for.  A more complete example might fetch
 // from an existing data source like a REST API or database.
-const books: any[] = [
+const books = [
     {
         title: "Harry Potter and the Chamber of Secrets",
         author: "J.K. Rowling",
@@ -11,7 +11,7 @@ const books: any[] = [
     {
         title: "Jurassic Park",
         author: "Michael Crichton",
-    },
+    }
 ];
 
 // Type definitions define the "shape" of your data and specify
